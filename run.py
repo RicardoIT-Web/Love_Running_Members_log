1517e2dc8c462ff8d13e932beb8bfddcff72ece8
RicardoIT-Web <rick_piedade@hotmail.com>
2021-12-02 11:28:08 +0000

    Add BMI calculation to application and provide user with options to find out more about BMI with a link to open a new tab, for more information on the topic

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
+import webbrowser
 
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+#     ]
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("love_running_members_log")
+# CREDS = Credentials.from_service_account_file("creds.json")
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open("love_running_members_log")
 
 
 def welcome_members_message():
@@ -48,34 +48,35 @@ while not lname.isalpha():
     lname = input("Error: please enter a last Name ")
 
 
-SHEET = GSPREAD_CLIENT.open("love_running_members_log")
+# SHEET = GSPREAD_CLIENT.open("love_running_members_log")
 
-WORKSHEET = SHEET.worksheet("members_details")
+# WORKSHEET = SHEET.worksheet("members_details")
 
-existing_user_fnames = WORKSHEET.col_values(1)
-existing_user_lnames = WORKSHEET.col_values(2)
+# existing_user_fnames = WORKSHEET.col_values(1)
+# existing_user_lnames = WORKSHEET.col_values(2)
 
 
-if fname not in existing_user_fnames:
-    WORKSHEET = SHEET.worksheet("members_details")
-    WORKSHEET.append_row([fname] + [lname])
+# if fname not in existing_user_fnames:
+#     WORKSHEET = SHEET.worksheet("members_details")
+#     WORKSHEET.append_row([fname] + [lname])
 
-if (fname) not in existing_user_fnames:
-    SHEET.add_worksheet(fname, 53, 20)
-    USER_WORKSHEET = SHEET.worksheet(fname)
-    USER_WORKSHEET.update(
-        "A1:I1",
-        [
-            ["Monday"],
-            ["Tuesday"],
-            ["Wednesday"],
-            ["Thursday"],
-            ["Friday"],
-            ["Saturday"],
-            ["Sunday"],
-            ["Weekly_Target"]
-        ],
-        major_dimension="COLUMNS")
+# if (fname) not in existing_user_fnames:
+#     SHEET.add_worksheet(fname, 53, 20)
+#     USER_WORKSHEET = SHEET.worksheet(fname)
+#     USER_WORKSHEET.update(
+#         "A1:I1",
+#         [
+#             ["Monday"],
+#             ["Tuesday"],
+#             ["Wednesday"],
+#             ["Thursday"],
+#             ["Friday"],
+#             ["Saturday"],
+#             ["Sunday"],
+#             ["Weekly_Target"],
+#             ["BMI Data"]
+#         ],
+#         major_dimension="COLUMNS")
 
 
 def welcome_message():
@@ -88,69 +89,91 @@ def welcome_message():
 
 welcome_message()
 
+# # prompt user is they'd like to provide a weekly target
+# weekly_target = input("Would you like to provide a weekly target? y/n: \n")
+# if weekly_target == "n":
+#     print(f"OK {fname} Let's move on...\n")
+# elif weekly_target == "y":
+#     weekly_target = input("How many KMs do you plan to run per week?\n")
 
-weekly_target = input("Would you like to provide a weekly target? y/n: \n")
-if weekly_target == "n":
+
+# def user_instructions():
+#     """
+#     Provide user with guidance of input responses
+#     """
+
+#     print("Please provide the distance of your daily runs. ")
+#     print("You should provide this data in numerical form. ")
+#     print("Example: 3.2 = 3.2km run, 5.0 = 5km run\n")
+#     print("If you did not run on a particular day, please type 0.\n ")
+
+
+# user_answers = []
+# weekdays = [
+#         "Monday",
+#         "Tuesday",
+#         "Wednesday",
+#         "Thursday",
+#         "Friday",
+#         "Saturday",
+#         "Sunday"
+#     ]
+# for weekday in weekdays:
+#     answer = input(f"How many kms did you run on {weekday}? \n")
+#     while True:
+#         try:
+#             float(answer)
+#             break
+#         except TypeError as e:
+#             print(f"{e}: {answer} is not a number!")
+#             test = input("Try another input:\n")
+#     user_answers.append(float(answer))
+
+# print(f"you have entered: {user_answers}\n")
+
+# # calculate total weekly distance run
+# sums = user_answers
+# print(f"Your total distance this week is: {sum(sums)}kms \n")
+
+
+# Prompt user if they'd like to calculate BMI and reveal calculated result
+bmi_data = input(f"{fname} Would you like to monitor your BMI? y/n: \n")
+if bmi_data == "n":
     print(f"OK {fname} Let's move on...\n")
-elif weekly_target == "y":
-    weekly_target = input("How many KMs do you plan to run per week?\n")
+
+elif bmi_data == "y":
+    height = float(input("Please enter your current height in cm: \n"))
+    weight = float(input("Please enter your current weight in kg: \n"))
+    bmi_data = weight/(height/100)**2
+    print(f"{fname} your BMI is currently {bmi_data} \n")
+    print("To find out more about BMI and how it is meassured; \n")
+    print("please click the following link:")
+
+    more_bmi_info = input("Want to know more about BMI & how it's calculated? y/n \n")
+    if more_bmi_info == "n":
+        print(f"OK {fname} Let's move on...\n")
+
+    elif more_bmi_info == "y":
+        webbrowser.open("https://www.nhs.uk/live-well/healthy-weight/bmi-calculator/")
 
 
-def user_instructions():
-    """
-    Provide user with guidance of input responses
-    """
-
-    print("Please provide the distance of your daily runs. ")
-    print("You should provide this data in numerical form. ")
-    print("Example: 3.2 = 3.2km run, 5.0 = 5km run\n")
-    print("If you did not run on a particular day, please type 0.\n ")
-
-
-user_answers = []
-weekdays = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-    ]
-for weekday in weekdays:
-    answer = input(f"How many kms did you run on {weekday}? ")
-    while True:
-        try:
-            float(answer)
-            break
-        except TypeError as e:
-            print(f"{e}: {answer} is not a number!")
-            test = input("Try another input:\n")
-    user_answers.append(float(answer))
-
-print(f"you have entered: {user_answers}\n")
-
-sums = user_answers
-print(f"Your total distance this week is: {sum(sums)}kms")
+# def update_members_log(weekday_answers, weekly_target_data):
+#     """
+#     Updating members log to the worksheet.
+#     Adding a new row with the list data provided
+#     """
+#     print("updating members log...\n")
+#     members_log_worksheet = SHEET.worksheet(fname)
+#     members_log_worksheet.append_row(weekday_answers)
+#     members_log_worksheet.update_acell("H2", weekly_target_data)
+#     print("Members log updated.\n")
 
 
-def update_members_log(weekday_answers, weekly_target_data):
-    """
-    Updating members log to the worksheet.
-    Adding a new row with the list data provided
-    """
-    print("updating members log...\n")
-    members_log_worksheet = SHEET.worksheet(fname)
-    members_log_worksheet.append_row(weekday_answers)
-    members_log_worksheet.update_acell("H2", weekly_target_data)
-    print("Members log updated.\n")
+# def main():
+#     """
+#     Run all program functions
+#     """
+#     update_members_log(user_answers, weekly_target)
 
 
-def main():
-    """
-    Run all program functions
-    """
-    update_members_log(user_answers, weekly_target)
-
-
-main()
+# main()
